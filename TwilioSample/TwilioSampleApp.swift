655605c4ac4c53f47bec330c6a04c1ee4c025207
Albert Kornas <albertkornas@gmail.com>
2021-12-29 21:34:02 -0500

    Initial Commit

---
+++ TwilioSample/TwilioSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwilioSampleApp.swift
+//  TwilioSample
+//
+//  Created by Albert Kornas on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct TwilioSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
